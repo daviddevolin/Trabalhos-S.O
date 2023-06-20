72ed1750b2c4ce4f3d5a6f5d261acc1f497ac24f
david santos <56547716+daviddevolin@users.noreply.github.com>
2023-06-20 14:08:25 -0300

    Update proc_init_prio_static.c

--- tarefa2.1/proc_init_prio_static.c
+++ tarefa2.1/proc_init_prio_static.c
@@ -10,6 +10,10 @@
 // Quantidade de processos a serem criados
 extern int NPROC;
 
+// variavel global que indica o tempo maximo que um processo pode executar ao todo
+extern int MAX_TIME;
+
+
 // Utilizando as variáveis globais das filas definidas no 'main'
 extern struct queue * ready;    // fila de aptos
 extern struct queue * ready2;   // segunda fila de aptos
@@ -20,17 +24,27 @@ extern struct queue * finished; // fila de finalizados
 void proc_init()
 {
     struct proc *p;
-
+    
     for (int i = 0; i < NPROC; i++) 
     {
         // criando um novo processo
         p = createproc();
-
+        
         // cria a thread do processo
         p->tid = createproc_thread(p);
 
-        // adicionando-o na fila de aptos
-        enqueue(ready, p);
+        // pegando o tempo a ser comparado         
+        int time_20 = MAX_TIME *0.2;
+
+        if (p->remaining_time<=time_20)
+        {
+            // adicionando-o na fila de aptos1
+            enqueue(ready, p);
+        }else
+        {
+            // adicionando-o na fila de aptos2
+            enqueue(ready2, p);
+        }
 
         // contabilizando a entrada do processo na fila de aptos
         count_ready_in(p);
